a4512f2717502e29839be218e894a8e4c04abcc2
ayersrijana697 <ayersrijana@697.in>
2026-02-24 18:00:15 +0545

    03_ operators.py

---
+++ 03_operators.py
@@ -0,0 +1,28 @@
+#arithmetic operators
+a=7
+b=4
+c=a+b
+print(c)
+
+# assignmeant operators
+a=4-2 # assign 4-2 in a
+print(a)
+b +=3 # increment the value of b by 3 and then assign it to b
+print(b)
+
+# comparision operators 
+
+d=5<4
+print(d)
+
+
+# logical operators
+
+e = True or False
+print(e)
+
+# truth table of or
+print("true or false is",True or False)
+print("true or true is",True or False)
+print("false or true is",False or True)
+print("false or false is",False or False)
